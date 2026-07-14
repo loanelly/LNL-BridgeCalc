b82aaeb3bbda197668b76ede0efc0c529a7d8481
lonly <egardshan@gmail.com>
2026-07-14 22:51:46 +0300

    Delete LNL-BridgeCalc directory

--- LNL-BridgeCalc/src/main/java/com/example/lnlbridgecalc/JarLauncher.java
+++
@@ -1,9 +0,0 @@
-package com.example.lnlbridgecalc;
-
-public class JarLauncher {
-    public static void main(String[] args) {
-        // Вызываем метод main основного класса приложения
-        // Это обходит проверку модулей JavaFX при старте JAR
-        HelloApplication.main(args);
-    }
-}
--- LNL-BridgeCalc/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.lnlbridgecalc {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.lnlbridgecalc to javafx.fxml;
-    exports com.example.lnlbridgecalc;
-}
